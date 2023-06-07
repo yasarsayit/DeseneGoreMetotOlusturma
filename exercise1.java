428a741e934e4faea3238126ae6eb8c87c85880e
yasarsayit <90604486+yasarsayit@users.noreply.github.com>
2023-06-07 20:43:10 +0300

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,28 @@
+package java101;
+
+import java.util.Scanner;
+
+public class exercise1 {
+
+	static int dgmo(int x, int tempX, boolean stop) {
+		System.out.print(x);
+		if (x < 0 || x == 0 || !stop) {
+			stop = false;
+			if (x != tempX) {
+				return dgmo(x + 5, tempX, stop);
+			} else
+				return 0;
+		} else {
+			return dgmo(x - 5, tempX, stop);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		int x, tempX;
+		System.out.print("Bir Sayı Giriniz: ");
+		x = inp.nextInt();
+		dgmo(x, tempX = x, true);
+	}
+
+}
